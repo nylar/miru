e7970705cec28f732fc6f1f7adab1c394fdce9b9
nylar <me@nylar.io>
2015-02-17 19:14:53 +0000

    Fix coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 echo "mode: set" > acc.out
-fail=0
-
-# Standard go tooling behavior is to ignore dirs with leading underscors
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d);
+for Dir in $(find ./* -maxdepth 10 -type d ); 
 do
-  if ls $dir/*.go &> /dev/null; then
-    go test -coverprofile=profile.out $dir || fail=1
-    if [ -f profile.out ]
+    if ls $Dir/*.go &> /dev/null;
     then
-      cat profile.out | grep -v "mode: set" >> acc.out
-      rm profile.out
+        returnval=`go test -coverprofile=profile.out $Dir`
+        echo ${returnval}
+        if [[ ${returnval} != *FAIL* ]]
+        then
+            if [ -f profile.out ]
+            then
+                cat profile.out | grep -v "mode: set" >> acc.out 
+            fi
+        else
+            exit 1
+        fi  
     fi
-  fi
 done
-
-# Failures have incomplete results, so don't send
-if [ -n "$COVERALLS" ] && [ "$fail" -eq 0 ]
+if [ -n "$COVERALLS_TOKEN" ]
 then
-  goveralls -v -coverprofile=acc.out $COVERALLS
-fi
+    goveralls -coverprofile=acc.out -service=wercker.com -repotoken $COVERALLS_TOKEN
+fi  
 
-rm -f acc.out
-
-exit $fail
+rm -rf ./profile.out
+rm -rf ./acc.out
